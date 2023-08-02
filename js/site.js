4246926988ebd4e1c6e057bb05a27eb3e8f14ef2
Adam Berry <aberry2288@gmail.com>
2023-08-02 10:01:00 -0400

    woking on js

--- js/site.js
+++ js/site.js
@@ -1,13 +1,49 @@
-function displayMessage() {
-    let inputBox = document.getElementById('message');
 
-    let message = inputBox.value;
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: message
-        }
-    );
+
+
+function getValues() {
+
+    let a = document.getElementById('loanAmount').value;
+    let b = document.getElementById('monthsAmount').value;
+    let c = document.getElementById('interestAmount').value;
+
+    a = parseFloat(a);
+    b = parseInt(b);
+    c = parseFloat(c);
+
+    topTableData(a, b, c);
+   
+
+}
+
+function displayData() {
+
+
+
+
+
+
+
+}
+
+function topTableData(loanAmount, term, interestRate) {
+
+    let totalPayment = loanAmount * (interestRate / 1200) / (1 - (1 + interestRate / 1200) ** -term);
+    let totalPrincipal = loanAmount
+    let totalInterest = (loanAmount * (interestRate * 0.01))/term;
+    let totalCost = loanAmount + totalInterest;
+    
+  
+    document.getElementById('monthlyPayments').innerHTML = totalPayment;
+    document.getElementById('totalPrincipal').innerHTML = totalPrincipal;
+    document.getElementById('totalInterest').innerHTML = totalInterest;
+    
+
+  
+
+    
+
+
+
 }
